c5472dceab4196dd9ee0ebb4a171c7cb7ae109f2
hcolina <slushball420@gmail.com>
2020-07-24 13:45:50 -0500

    explanatory comment

--- temperature-converter.py
+++ temperature-converter.py
@@ -5,6 +5,8 @@ def fahrenheit_to_celcius():
     celsius = (5/9) * (float(fahrenheit) - 32)
     lbl_result["text"] = f"{round(celsius, 2)} \N {DEGREE CELSIUS}"
 
+#window set-up
 window = tk.Tk()
 window.title("Temperature Converter")
 window.resizable(width=False, height=False)
+
