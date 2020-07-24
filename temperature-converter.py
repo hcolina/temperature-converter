5200444ab2f7b8a90e5469695e9b1a67218a838b
hcolina <slushball420@gmail.com>
2020-07-24 15:00:23 -0500

    syntax

--- temperature-converter.py
+++ temperature-converter.py
@@ -16,5 +16,5 @@ frm-entry = tk.Frame(master=window)
 ent_temperature = tk.Entry(master=frm_entry, width=10)
 lbl_temp = tk.Label(master=frm_entry, text="\N{DEGREE FAHRENHEIT}")
 
-ent_temperature.grid(row=0, column=0, sticky=e)
+ent_temperature.grid(row=0, column=0, sticky="e")
 
