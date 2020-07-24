232b1f67590b23281b0fad607f53a3b86c02f593
hcolina <slushball420@gmail.com>
2020-07-24 13:32:47 -0500

    open new window

--- temperature-converter.py
+++ temperature-converter.py
@@ -4,3 +4,5 @@ def fahrenheit_to_celcius():
     fahrenheit = ent_temperature.get()
     celsius = (5/9) * (float(fahrenheit) - 32)
     lbl_result["text"] = f"{round(celsius, 2)} \N {DEGREE CELSIUS}"
+
+window = tk.Tk()
