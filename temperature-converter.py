338e7efb597265e5e7ee759898bc531674f69b55
hcolina <slushball420@gmail.com>
2020-07-24 13:30:42 -0500

    remove explanatory comment

--- temperature-converter.py
+++ temperature-converter.py
@@ -1,8 +1,6 @@
 import tkinter as tk
 
 def fahrenheit_to_celcius():
-        '''This converts the Fahrenheit value into Celcius and inputs the
-        result into the lbl_result'''
     fahrenheit = ent_temperature.get()
     celsius = (5/9) * (float(fahrenheit) - 32)
     lbl_result["text"] = f"{round(celsius, 2)} \N {DEGREE CELSIUS}"
