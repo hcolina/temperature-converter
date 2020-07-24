1cde5761a17118a6a0d11c0fafe39550d6a0ed3e
hcolina <slushball420@gmail.com>
2020-07-24 15:09:25 -0500

    call tk

--- temperature-converter.py
+++ temperature-converter.py
@@ -20,4 +20,4 @@ lbl_temp = tk.Label(master=frm_entry, text="\N{DEGREE FAHRENHEIT}")
 ent_temperature.grid(row=0, column=0, sticky="e")
 lbl_temp.grid(row=0, column=1, sticky="w")
 
-btn_convert = 
+btn_convert = tk.Button()
